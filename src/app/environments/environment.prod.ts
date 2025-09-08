2a3ed1c7868ede5324d08eb17448e04f0a3bbaeb
Pam rosas <pam.latasoft@gmail.com>
2025-09-08 15:11:03 -0300

    se agrega archivo de redirecciones

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: true,
-    apiUrl: 'https://backend-psicologia-fckw.onrender.com/api'
+    production: false,
+    apiUrl: 'http://localhost:3000/api'
   };
   
