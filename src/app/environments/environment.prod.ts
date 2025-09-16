239c66b249ceaa76d163243b28b67a43f32cf556
Tiare Caamaño <ti.caamano@duocuc.cl>
2025-09-16 12:32:49 -0300

    actualizacion

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,17 +1,5 @@
-// const environment = {
-    
-//*<<<<<<< HEAD
-   //* production: true,
-   //* apiUrl: 'http://localhost:3000'
-//+=======
-   //* production: false,
-    //*apiUrl: 'http://localhost:3000/api'
-//*>>>>>>> 5fd815d7d26041a7096c49eb8345230679768a3c
- //*
- //  };
-
- export const environment = {
+export const environment = {
     production: true,
-    apiUrl: 'http://localhost:3000'
+    apiUrl: 'https://psicoterapia-backend.onrender.com'
 };
   
